935c05741ff0b22377307d30ecc1a55a1b5745c5
Jose-Prince <joseprince148mail.com>
2023-04-20 10:08:02 -0600

    TreeFactory
    Se crea el factory para los arboles

---
+++ TreeFactory.java
@@ -0,0 +1,23 @@
+public class TreeFactory {
+    
+    public iTree getInstance(int num){
+        iTree ArbolUsado = null;
+
+        switch (num) {
+            case 1:
+                ArbolUsado = new SplayTree();
+                break;
+        
+            case 2:
+                ArbolUsado = new RedBlackTree();
+                break;
+
+            case 3:
+                ArbolUsado = new AVLTree();
+                break;
+        }
+
+        return ArbolUsado;
+    }
+    
+}
