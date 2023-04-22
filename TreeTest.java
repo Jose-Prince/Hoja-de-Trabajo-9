6231adce1c8dd6ba4d08bda7f1f487a7f6edfc89
Jose-Prince <joseprince148mail.com>
2023-04-21 21:45:50 -0600

    Test y vocabulario
    Se realizan las pruebas unitarias de los arboles sobre las funciones de insert y search. Se agregan nuevas palabras a Spanish.txt

---
+++ TreeTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TreeTest {
+
+    iTree splay = new SplayTree();
+    iTree rbt = new RedBlackTree();
+    iTree avl = new AVLTree();
+    
+    @Test
+    public void SplayInsertTest(){
+        splay.insert("word,palabra");
+        assertEquals("palabra", splay.get("word"));
+
+    }
+
+    @Test
+    public void RBTInsertTest() {
+        splay.insert("word,palabra");
+        assertEquals("palabra", splay.get("word"));
+    }
+
+    @Test 
+    public void AVLInsertTest() {
+        splay.insert("word,palabra");
+        assertEquals("palabra", splay.get("word"));
+    }
+
+    @Test 
+    public void SplaySearchTest() {
+        splay.insert("word,palabra");
+        assertEquals(true, splay.search("word"));
+    }
+
+    @Test
+    public void RBTSearchTest() {
+        splay.insert("word,palabra");
+        assertEquals(true, splay.search("word"));
+    }
+
+    @Test
+    public void AVLSearchTest() {
+        splay.insert("word,palabra");
+        assertEquals(true, splay.search("word"));
+    }
+}
